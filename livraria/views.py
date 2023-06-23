a5baee258c51dcbd669da402e39f32c31831cb65
thesh1ro <gabrielmonteiro1111@gmail.com>
2023-06-23 14:25:40 -0300

    Default isAuthenticated permissions

--- livraria/views.py
+++ livraria/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from livraria.models import Autor, Categoria, Editora, Livro
@@ -19,6 +20,7 @@ class AutorViewSet(ModelViewSet):
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+    # permission_classes = [IsAuthenticated]
 
 
 class EditoraViewSet(ModelViewSet):
